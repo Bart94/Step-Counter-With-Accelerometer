2c8644dc79f48f4877b0ef30276e37e8cf7208be
Bartolomeo Elia <b.elia@studenti.unisa.it>
2017-11-20 17:16:58 +0100

    Initial commit

---
+++ app/src/main/java/com/example/bcoccaro/cwa/CoordPoint.java
@@ -0,0 +1,11 @@
+package com.example.bcoccaro.cwa;
+
+public class CoordPoint {
+
+    float px, py;
+
+    public CoordPoint(float px, float py) {
+        this.px = px;
+        this.py = py;
+    }
+}
---
+++ app/src/main/java/com/example/bcoccaro/cwa/MovingAverage.java
@@ -0,0 +1,29 @@
+package com.example.bcoccaro.cwa;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MovingAverage {
+
+    private static float filterSum = 0;
+    private static float filterResult = 0;
+    private final static Queue<Float> maWindow = new LinkedList<Float>();
+
+    public static float movingAverage(float accModule, int length) {
+
+        filterSum += accModule;
+        maWindow.add(accModule);
+
+        if (maWindow.size() > length) {
+            float head = maWindow.remove();
+            filterSum -= head;
+        }
+        if (!maWindow.isEmpty()) {
+            filterResult = filterSum / maWindow.size();
+        }
+
+        return filterResult;
+    }
+
+}
